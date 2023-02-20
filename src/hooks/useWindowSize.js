7ae58c13385e33a46adbf78adacf53aa42b38689
AnyaKononchenko <anya.kononchenko@gmail.com>
2023-02-20 20:03:15 +0100

    cleaned code a lil bit

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function getWindowSize() {
     const { innerWidth: width, innerHeight: height } = window;
