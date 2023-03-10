81f278c95414443c324caa7484293553d9f2f0a7
AnyaKononchenko <anya.kononchenko@gmail.com>
2023-03-10 19:37:01 +0100

    updated timeout

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,32 +4,36 @@ const useFetch = (url) => {
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
-
+   
     useEffect(() => {
         const abortController = new AbortController();
-        fetch(url, { signal: abortController.signal })
-            .then(res => {
-                if (!res.ok) { // error coming back from server
-                    throw Error('Could not fetch the data for that resource');
-                }
-                return res.json();
-            })
-            .then(data => {
-                setIsPending(false);
-                setData(data);
-                setError(null);
-            })
-            .catch(err => {
-                // auto catches network / connection error
-                if (err.name === 'AbortError') {
-                    console.log('fetch aborted');
-                } else {
+        setTimeout(() => { // for the pure sake of loading logos displayin
+            fetch(url, { signal: abortController.signal })
+                .then(res => {
+                    if (!res.ok) { // error coming back from server
+                        throw Error('Could not fetch the data for that resource');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    console.log(data);
                     setIsPending(false);
-                    setError(err.message);
-                }
-            })
+                    setData(data);
+                    setError(null);
+                })
+                .catch(err => {
+                    // auto catches network / connection error
+                    if (err.name === 'AbortError') {
+                        console.log('fetch aborted');
+                    } else {
+                        setIsPending(false);
+                        setError(err.message);
+                    }
+                })
+        }, 800);
         return () => abortController.abort();
     }, [url]);
+
 
     return { data, isPending, error };
 }
